fb0a8ed25481bbce304aa7f8cc9c643993725c79
huangbh <578092215@qq.com>
2023-10-22 08:25:14 +0800

    :sparkles: feat(custom): 增加分类页

---
+++ pages/categories/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<main class="container max-w-5xl mx-auto text-zinc-600">
+		<CategoryHero />
+		<div class="flex flex-wrap px-6 mt-12 gap-3">
+			<template v-for="topic in topics" :key="topic">
+				<CategoryCard :title="topic" />
+			</template>
+		</div>
+	</main>
+</template>
+<script lang="ts" setup>
+const topics = [
+	"javascript",
+	"typescript",
+	"git",
+	"docker",
+	"kubernetes",
+	"vue",
+	"nuxt",
+	"pinia",
+	"vuex",
+	"firebase",
+	"supabse",
+	"cypress",
+	"vercel",
+	"namecheap"
+];
+</script>
