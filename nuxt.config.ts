7b38e2a9d8e455de958b4d31aa8953d9ff980be6
huangbh <578092215@qq.com>
2023-10-24 17:30:51 +0800

    :zap: perf(custom): 优化seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,8 +11,16 @@ export default defineNuxtConfig({
 	],
 	app: {
 		pageTransition: { name: "page", mode: "out-in" },
-		layoutTransition: { name: "layout", mode: "out-in" }
+		layoutTransition: { name: "layout", mode: "out-in" },
+		head: {
+			charset: "utf-8",
+			viewport: "width=device-width,initial-scale=1",
+			title: "黄某人's Blog",
+			meta: [{ name: "description", content: "黄某人的博客" }]
+		}
 	},
+	typescript: { strict: true },
+
 	colorMode: {
 		preference: "system",
 		fallback: "light",
