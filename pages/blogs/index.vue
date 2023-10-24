9ce950a9f206e956f5fe65994461901f43e550fe
huangbh <578092215@qq.com>
2023-10-24 15:44:28 +0800

    :lipstick: style(custom): 优化数据加载

--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -9,26 +9,27 @@
 				class="block w-full bg-[#F1F2F4] dark:bg-zinc-800 dark:placeholder-zinc-500 text-zinc-300 rounded-md border-gray-300 dark:border-gray-800 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
 			/>
 		</div>
-		<ClientOnly>
-			<div v-auto-animate class="space-y-5 my-5">
-				<template v-for="post in postData" :key="post.title">
-					<BlogArchive
-						:path="post.path"
-						:title="post.title"
-						:created-at="post.createdAt"
-						:description="post.description"
-						:image="post.image"
-						:alt="post.alt"
-						:tags="post.tags"
-					/>
-				</template>
-				<BlogArchive v-if="postData.length <= 0" />
-			</div>
-			<template #fallback>
+		<div v-auto-animate class="space-y-5 my-5">
+			<template v-if="pending">
 				<BlogLoader />
 				<BlogLoader />
 			</template>
-		</ClientOnly>
+			<template v-else>
+				<BlogArchive
+					v-for="post in postData"
+					:key="post.title"
+					:path="post.path"
+					:title="post.title"
+					:created-at="post.createdAt"
+					:description="post.description"
+					:image="post.image"
+					:alt="post.alt"
+					:tags="post.tags"
+				/>
+				<BlogArchive v-if="postData.length === 0" />
+			</template>
+		</div>
+
 		<div class="flex justify-center items-center space-x-6">
 			<button :disabled="page <= 1" @click="onPreviousPageClick">
 				<Icon
@@ -53,7 +54,7 @@ import dayjs from "dayjs";
 const size = ref(3);
 const page = ref(1);
 const keyword = ref("");
-const { data, refresh } = await useAsyncData("postList", () =>
+const { data, refresh, pending } = await useAsyncData("postList", () =>
 	$fetch("/api/blog", {
 		method: "GET",
 		query: { page: page.value, size: size.value, keyword: keyword.value }
