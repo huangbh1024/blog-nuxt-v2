8c0a119afad6a3513bbc4699add34223c9344ac6
huangbh <578092215@qq.com>
2023-10-24 23:13:09 +0800

    :lipstick: style(custom): 增加暗色

--- components/category/topic.vue
+++ components/category/topic.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="container mx-auto">
-		<div class="p-6 my-4 mx-3 rounded-md bg-gray-200">
+		<div class="p-6 my-4 mx-3 rounded-md bg-gray-200 dark:bg-zinc-800">
 			<h1
-				class="text-black font-semibold leading-tight text-4xl md:text-5xl my-5"
+				class="text-black dark:text-zinc-300 font-semibold leading-tight text-4xl md:text-5xl my-5"
 			>
 				#{{ category }}
 			</h1>
