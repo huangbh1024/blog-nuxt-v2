4779d90400ce48adc61cee7765e4a372cbc81c86
huangbh <578092215@qq.com>
2023-10-26 08:45:52 +0800

    :sparkles: feat(custom): 归档添加跳转

--- pages/archives.vue
+++ pages/archives.vue
@@ -2,7 +2,7 @@
 	<main class="container max-w-5xl mx-auto text-zinc-600">
 		<ArchiveHero />
 		<ul class="px-12">
-			<template v-for="item in data" :key="item.year">
+			<template v-for="item in archiveData" :key="item.year">
 				<div class="select-none relative h-20 pointer-events-none">
 					<span
 						class="text-[8em] text-transparent absolute -left-12 -top-8 font-bold text-stroke-2 text-stroke-hex-aaa opacity-10"
@@ -10,12 +10,20 @@
 					>
 				</div>
 				<div v-for="blog in item.blogs" :key="blog.id">
-					<NuxtLink class="block font-normal mb-6 mt-2 no-underline">
+					<NuxtLink
+						class="block font-normal mb-6 mt-2 no-underline cursor-pointer hover:underline"
+						:to="blog.path"
+					>
 						<li
 							class="no-underline flex flex-col md:flex-row gap-2 md:items-center"
 						>
 							<div class="title text-lg leading-[1.2em] flex gap-2 flex-wrap">
 								<span class="align-middle">{{ blog.title }}</span>
+							</div>
+							<div class="flex gap-2 items-center">
+								<span class="text-sm opacity-50 whitespace-nowrap">{{
+									blog.createdAt
+								}}</span>
 							</div>
 						</li>
 					</NuxtLink>
@@ -26,8 +34,20 @@
 </template>
 
 <script lang="ts" setup>
+import dayjs from "dayjs";
 const { data } = await useAsyncData("archives", () =>
 	$fetch("/api/blog/archive", { method: "GET" })
+);
+const archiveData = computed(
+	() =>
+		data.value?.map(item => ({
+			...item,
+			blogs: item.blogs.map(blog => ({
+				...blog,
+				path: `/blogs/${blog.id}`,
+				createdAt: dayjs(blog.createdAt).format("YYYY-MM-DD")
+			}))
+		})) ?? []
 );
 </script>
 
